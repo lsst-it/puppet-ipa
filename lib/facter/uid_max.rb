46ad61d94810c169269320c38b885950cbc26fc0
Joshua Hoblitt <josh@hoblitt.com>
2023-10-25 15:23:22 -0700

    rm uid_max & gid_max facts
    These facts are unnecessary for declarative puppet code.

--- lib/facter/uid_max.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Facter.add(:uid_max) do
-  setcode do
-    lines = File.readlines('/etc/login.defs')
-    lines.find { |line| line.start_with?('UID_MAX') }.split[1].strip.to_i
-  end
-end
