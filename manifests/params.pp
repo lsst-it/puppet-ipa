acffd3b930b8e9d1f3b234997c5f708451580a5c
Joshua Hoblitt <josh@hoblitt.com>
2023-10-25 15:20:38 -0700

    drop support for debian 10 -- no freeipa-client package

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,8 +15,12 @@ class easy_ipa::params {
     }
     'Debian': {
       case $facts['os']['distro']['codename'] {
-        /(trusty|xenial|bionic|buster|focal|bullseye|jammy)/: { $ipa_client_package_ensure = 'present' }
-        default:                                              { fail('ERROR: unsupported operating system') }
+        /(trusty|xenial|bionic|focal|bullseye|jammy)/: {
+          $ipa_client_package_ensure = 'present'
+        }
+        default: {
+          fail('ERROR: unsupported operating system')
+        }
       }
       $ipa_client_package_name = 'freeipa-client'
     }
