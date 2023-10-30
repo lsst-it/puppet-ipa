43e86ce229c9266ba47803dd3b7ca81255168b6e
Joshua Hoblitt <josh@hoblitt.com>
2023-10-30 10:29:09 -0700

    rm Vagrantfile

--- vagrant/centos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo Final provisioning...
-export FACTER_is_vagrant='true'
-
-yum -y upgrade
-
-# Recent CentOS images have NetworkManager enabled. As it breaks IPA server's
-# /etc/resolv.conf we don't want to use it.
-puppet apply -e "service { 'NetworkManager': ensure => 'stopped', enable => false, }"
